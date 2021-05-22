b66b4157e2938c1bc3e17022fe2d6108318e871f
Thomas Elder <MasonCosLive@Mason-Cos-Macbook.local>
2021-05-22 11:51:30 -0600

    Added contact page

--- tj-portfolio/src/App.js
+++ tj-portfolio/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
 import About from "./pages/about";
+import Contact from "./pages/contact";
 import NoMatch from "./pages/NoMatch";
 import TopBar from "./components/Nav";
 
@@ -14,6 +15,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+
           <Route component={NoMatch} />
         </Switch>
       </div>
