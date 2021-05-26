c7fa67593611486661c36530745648eeff913d03
Thomas Elder <MasonCosLive@Mason-Cos-Macbook.local>
2021-05-26 10:08:46 -0600

    Fixed linking to use react router dom throughout site

--- tj-portfolio/src/components/Hero/index.js
+++ tj-portfolio/src/components/Hero/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import "./hero.css";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
     <div id="hero">
       <div className="hero-text">
         <h1>TJ Elder - Web Developer</h1>
-        <Button href="/work" id="work-button" variant="outline-dark">
-          View My Work
-        </Button>
+        <Link to="/work">
+          {" "}
+          <Button id="work-button" variant="outline-dark">
+            View My Work
+          </Button>
+        </Link>
       </div>
     </div>
   );
